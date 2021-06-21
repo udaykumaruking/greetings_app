9427f2d9092b93644bf1f1ce47f49e9c04cd6570
Uday Kumar Madarapu <udaykumaruking@gmail.com>
2021-06-21 12:18:35 +0530

    Adding Data

---
+++ lib/data.dart
@@ -0,0 +1,24 @@
+class Data {
+  List data = [
+    [
+      './assets/images/good_morning.png',
+      'Good Morning',
+      'A New Day, A New Blessing, A New Hope',
+    ],
+    [
+      './assets/images/good_afternoon.png',
+      'Good Afternoon',
+      'May this afternoon wish refresh you and make the upcoming moments count. Have a great afternoon.',
+    ],
+    [
+      './assets/images/good_evening.png',
+      'Good Evening',
+      'Evenings are for relaxing with a cup of tea and preparing yourself for tomorrow.',
+    ],
+    [
+      './assets/images/good_night.png',
+      'Good Night',
+      'Relax your soul and have a peaceful sleep.',
+    ],
+  ];
+}
