6aaff8f6a6b79e6a3b64727e5aa8103bbff0d968
Uday Kumar Madarapu <udaykumaruking@gmail.com>
2021-06-21 09:44:02 +0530

    Adding constants and packages

---
+++ lib/constants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color.fromRGBO(248, 250, 252, 1);
+const Color kDarkGrey = Color.fromRGBO(15, 23, 45, 1);
+const Color kNormalGrey = Color.fromRGBO(51, 65, 85, 1);
+
+const TextStyle kTitle = TextStyle(
+  color: kDarkGrey,
+  fontWeight: FontWeight.w400,
+  letterSpacing: 0.32,
+  fontSize: 26,
+);
+
+const TextStyle kHeading = TextStyle(
+  color: kDarkGrey,
+  fontWeight: FontWeight.w500,
+  fontSize: 24,
+);
+
+const TextStyle kParagraph = TextStyle(
+  color: kNormalGrey,
+  fontWeight: FontWeight.w300,
+  letterSpacing: 0.32,
+  fontSize: 16,
+);
+
+const double kPadding = 16;
